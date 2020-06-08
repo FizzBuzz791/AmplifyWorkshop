2354bdfa082bde5c81f67d45425b1662ff99815e
Trent Jones <trentwilliamjones@gmail.com>
2020-06-08 14:22:07 +0800

    Auth - frontend

--- photoalbums/src/App.js
+++ photoalbums/src/App.js
@@ -1,9 +1,22 @@
 import React from "react";
 
+import Amplify from "aws-amplify";
+import aws_exports from "./aws-exports";
+
+import { withAuthenticator } from "aws-amplify-react";
 import { Header } from "semantic-ui-react";
+
+import "@aws-amplify/ui/dist/style.css";
+
+Amplify.configure(aws_exports);
 
 function App() {
   return <Header as="h1">Hello World!</Header>;
 }
 
-export default App;
+export default withAuthenticator(App, {
+  includeGreetings: true,
+  signUpConfig: {
+    hiddenDefaults: ["phone_number"],
+  },
+});
